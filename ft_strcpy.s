5003be3475093e0107098bf91183c3c106c3e1ce
lvirgini <lvirgini@student.42.fr>
2020-04-02 14:52:33 +0200

    e

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,15 +1,2 @@
-global			_ft_strcpy
 
-			section			.text
-_ft_strcpy:	
-			mov				rax, rdi
-cpy_one:
-			cmp				byte [rsi], 0
-			jl				return
-			mov				r8b, byte [rsi]
-			mov				byte [rdi], r8b
-			inc				rsi
-			inc				rdi
-			jmp				cpy_one
-return:
-			ret
+	global			ft_strcpy
